60a5a120d19be2defc7e8abbfefd001e5f579fd9
RajuJanne <24858501+RajuJanne@users.noreply.github.com>
2025-12-27 20:14:51 +0200

    Github Desktop is very bad at detecting diffs, always use Git instead.

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import SetupView from '../views/SetupView.vue'
+import DraftView from '../views/DraftView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/', component: () => import('../App.vue') }],
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: SetupView },
+    { path: '/draft/:id', component: DraftView, props: true }
+  ],
 })
-
 export default router
